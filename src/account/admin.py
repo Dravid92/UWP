cef5017054d3271d0d3aa56729c7c8bbf039a2e4
Dravid Hemanth <dravidhemanth18@gmail.com>
2020-05-12 15:20:56 +0530

    Added search bar to users in admin panel , removed question tab

--- src/account/admin.py
+++ src/account/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
 from account.models import Account
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(Account)
-# Register your models here.
+class AccountAdmin(UserAdmin):
+	#displayed in the columns of each users
+	list_display = ( 'email','username','date_joined','last_login','is_admin','is_staff')
+	search_fields = ('email','username',)
+	readonly_fields = ('date_joined','last_login')
+
+	filter_horizontal = ()
+	list_filter = ()
+	fieldsets = ()
+admin.site.register(Account,AccountAdmin)
+
